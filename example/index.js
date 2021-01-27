0573ab99e7f2006eaad52e7dd2823c1cf6491814
Abiodun Adeyinka <adeyinkab24@gmail.com>
2021-01-27 22:38:26 +0100

    version 1.0.5

--- example/index.js
+++ example/index.js
@@ -1,29 +1,29 @@
-const Validator  = require('../src');
+const Validator  = require("../src");
 
 let rules = {
-    name: 'required|min:3|max:10|alpha',
-    username: 'required|min:3|max:10|alpha_num',
-    email: 'required|email',
-    lastName: 'required_if:name,naomi',
-    password: 'required|min:8|max:25|alpha_num_punct|confirmed:password_confirmation',
-    password_confirmation: 'required',
-    pin: 'required|number'
+    name: "required|min:3|max:10|alpha",
+    username: "required|min:3|max:10|alpha_num",
+    email: "required|email",
+    lastName: "required_if:name,naomi",
+    password: "required|min:8|max:25|alpha_num_punct|confirmed:password_confirmation",
+    password_confirmation: "required",
+    pin: "required|number"
 };
 
 
 let validate = new Validator(rules);
 
 form = {
-    name: 'naomi',
-    email: 'olagmailcom',
-    password: 'passcom123@',
+    name: "naomi",
+    email: "olagmailcom",
+    password: "passcom123@",
     lastName: null,
-    password_confirmation: 'passcom123@',
-    pin: '2333',
-    username: 'a'
+    password_confirmation: "passcom123@",
+    pin: "2333",
+    username: "a"
 };
 
-validate.validate('name', form.name).then( (success) => {
+validate.validate("name", form.name).then( (success) => {
     // success is a true/false value. If validation pass
     if(success){
         //do something
@@ -31,15 +31,15 @@ validate.validate('name', form.name).then( (success) => {
         //do something
     }
 });
-validate.validate('email', form.email);
-validate.validate('password', form.password);
+validate.validate("email", form.email);
+validate.validate("password", form.password);
 validate.validateAll(form);
 const {errors} = validate;
 
 
-console.log( errors.has('name'), errors.first('name'));
-console.log( errors.has('email'), errors.first('email'));
-console.log( errors.has('lastName'), errors.first('lastName'));
-console.log( errors.has('password'), errors.first('password'));
-console.log( errors.has('username'), errors.first('username'));
-console.log( errors.has('pin'), errors.first('pin'));
+console.log( errors.has("name"), errors.first("name"));
+console.log( errors.has("email"), errors.first("email"));
+console.log( errors.has("lastName"), errors.first("lastName"));
+console.log( errors.has("password"), errors.first("password"));
+console.log( errors.has("username"), errors.first("username"));
+console.log( errors.has("pin"), errors.first("pin"));
